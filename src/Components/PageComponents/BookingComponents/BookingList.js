6c34642627c420557ce0e2ced40a3607ee35f341
Afridh <afridh7788@gmail.com>
2022-09-29 23:40:34 -0400

    fetched data to booking.js

--- src/Components/PageComponents/BookingComponents/BookingList.js
+++ src/Components/PageComponents/BookingComponents/BookingList.js
@@ -3,20 +3,42 @@ import classes from './BookingList.module.css'
 import {Button} from 'react-bootstrap' 
 import {useNavigate} from 'react-router-dom'
 import { collection, getDoc, onSnapshot,doc } from 'firebase/firestore'
-import {db} from '../../../Firebase'
-// import {useUserAuth} from '../../../Context/Context'
+import {db,auth} from '../../../Firebase'
+import {useUserAuth} from '../../../Context/Context'
+import { onAuthStateChanged } from 'firebase/auth'
 
 function BookingList() {
 
 const navigate  = useNavigate('')
 const [bookings,setBookings] = useState([])
 const [error,setError] = useState()
-const [tourGuideName, setTourGuideName] = useState('')
-// const {user} = useUserAuth()
+const [tourGuideDetails, setTourGuideDetails] = useState('')
+const [touristDetails,setTouristDetails] = useState('')
+const [tourist,setTourist] = useState('')
+const [tourGuide,setTourGuide] = useState('')
+
+const {user} = useUserAuth()
 
 useEffect(()=>{
-    const booking = onSnapshot(collection(db,'booking'),(snapshot)=>{
-        let list  =[]
+    booking()
+},[])
+
+    onAuthStateChanged(auth, async (user)=>{
+        if(user){
+            const tourists = await getDoc(doc(db,'Tourists',user.uid))
+            const touristData = tourists.data()
+            const tourGuides = await getDoc(doc(db,'Tour_Guides',user.uid))
+            const tourGuideData = tourGuides.data()
+    
+            if(touristData==undefined){
+              setTourGuide(touristDetails)
+            }else if(tourGuideData==undefined){
+              setTourist(tourGuideDetails)
+            }
+          }
+    })
+    const booking = onSnapshot(collection(db,'booking'),((snapshot)=>{
+        let list = []
         snapshot.docs.forEach((doc)=>{
             list.push({
                 id:doc.id,
@@ -24,67 +46,74 @@ useEffect(()=>{
             })
         })
         setBookings(list)
-        {bookings.map((booking)=>{
-            // console.log(booking.tourGuideId)
-            const tourGuide = getDoc(doc(db,'Tour_Guides',booking.tourGuideId))
-            const tourGuideData = tourGuide.data()
-            console.log(tourGuideData)
+        list.forEach((item)=>{
+            const tourGuide = getDoc(doc(db,'Tour_Guides',item.tourGuideId))
+            .then((data)=>{
+                setTourGuideDetails(data.data())
+            })
 
-        })}
-    },(error)=>{
-        setError(error.message)
-    })
-    return ()=>{
-        booking()
-    }
-    // printName()
-},[])
-
+            const tourist = getDoc(doc(db,'Tourists',item.touristId))
+            .then((data)=>{
+                setTouristDetails(data.data())
+            })          
+        })
+    }))
+    
 function handleBooking(){
     navigate('/selectedBooker')
     
 }
 
-// useEffect(()=>{
-//     const tourGuide = onSnapshot(collection(db,'Tour_Guides'))
-//     const tourGuideData = tourGuide.data()
-//     console.log({tourGuideData})
-// })
-    
-
-
-// function printName(id){
-//     // bookings.map((booking)=> {
-//         //  const tourGuide = getDoc(doc(db,'Tour_Guides',id))
-//         //  const tourGuideData = tourGuide.data()
-//         //  console.log(id)
-//     //     setTourGuideName(tourGuideData.name)
-//     return 'name'
-//     // })
-// }
-
-  return (
-    <div className = {classes.bookingContainer}>
-        {bookings.map((booking)=>(
-            <div className = {classes.bookingList} key = {booking.id}>
-            <img src = 'https://firebasestorage.googleapis.com/v0/b/ceylon-assistant.appspot.com/o/Tour_Guide_Images%2Fff2ab708-d1ef-4400-9c02-1adabd3a2a27.jpg428f69cd-af41-4d8b-b7dc-fa46fdcb994c?alt=media&token=aabcbf7d-4f43-47da-9cef-674db5357da5' 
-            alt = '' />
-            <div className = {classes.detailsContainer}>
-                <div className = {classes.bookingDetails}>
-                    <span><b>Name : </b></span>
-                    <span><b>Tour Location : </b>{booking.travel_location} </span>
-                    <span><b>Pick-up Destination : </b>{booking.pickup_location}</span>
-                    <span><b>Tour Date Range : </b></span>
-                    <span><b>Pick-up Time : </b>{booking.time}</span>
-                </div>
-                <div className = {classes.buttonContainer}>
-                    <Button onClick = {handleBooking}>More Details</Button>
+if(tourist){
+    return (
+        <div className = {classes.bookingContainer}>
+            {bookings.map((booking)=>(
+                <div className = {classes.bookingList} key = {booking.id}>
+                <img src = {tourist.image} 
+                alt = '' />
+                <div className = {classes.detailsContainer}>
+                    <div className = {classes.bookingDetails}>
+                        <span><b>Name : </b>{tourist.name}</span>
+                        <span><b>Tour Location : </b>{booking.travel_location} </span>
+                        <span><b>Pick-up Destination : </b>{booking.pickup_location}</span>
+                        <span><b>Tour Date Range : </b></span>
+                        <span><b>Pick-up Time : </b>{booking.time}</span>
+                    </div>
+                    <div className = {classes.buttonContainer}>
+                        <Button onClick = {handleBooking}>More Details</Button>
+                    </div>
                 </div>
             </div>
+            ))}
         </div>
-        ))}
-    </div>
-  )
+      )
+}
+
+if(tourGuide){
+    return (
+        <div className = {classes.bookingContainer}>
+            {bookings.map((booking)=>(
+                <div className = {classes.bookingList} key = {booking.id}>
+                <img src = {tourGuide.image} 
+                alt = '' />
+                <div className = {classes.detailsContainer}>
+                    <div className = {classes.bookingDetails}>
+                        <span><b>Name : </b>{tourGuide.name}</span>
+                        <span><b>Tour Location : </b>{booking.travel_location} </span>
+                        <span><b>Pick-up Destination : </b>{booking.pickup_location}</span>
+                        <span><b>Tour Date Range : </b></span>
+                        <span><b>Pick-up Time : </b>{booking.time}</span>
+                    </div>
+                    <div className = {classes.buttonContainer}>
+                        <Button onClick = {handleBooking}>More Details</Button>
+                    </div>
+                </div>
+            </div>
+            ))}
+        </div>
+      )
+}
+
 }
 
 export default BookingList;
